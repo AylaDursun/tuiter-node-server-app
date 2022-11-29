89947246cc3f7ff61e7d91a15c9e1aa9bb312840
AylaDursun <dursun.a@northeastern.edu>
2022-11-29 13:51:48 -0500

    A9 add additional info fields

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,14 @@ const schema = mongoose.Schema({
                                    tuit: String,
                                    likes: Number,
                                    liked: Boolean,
+                                   dislikes: Number,
+                                   disliked: Boolean,
+                                   retuits: Number,
+                                   replies: Number,
+                                   topic: String,
+                                   time: String,
+                                   username: String,
+                                   handle: String,
+                                   image: String
                                }, {collection: 'tuits'});
 export default schema;
